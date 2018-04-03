4e268cdd8a63cfb923105cea592859984fc5e920
Mateusz Borowski <borowa5b@gmail.com>
2018-04-03 16:57:12 +0200

    Updated Authors & Help Activity

--- QuizUZ/app/src/main/java/uz/pl/quizuz/GameHelp.java
+++ QuizUZ/app/src/main/java/uz/pl/quizuz/GameHelp.java
@@ -2,12 +2,14 @@ package uz.pl.quizuz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
  * Game help class
+ *
  * @author Mateusz Borowski
  */
 public class GameHelp extends AppCompatActivity {
@@ -16,9 +18,10 @@ public class GameHelp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_help);
 
-        TextView tv = new TextView(this);
-        tv.setText("Tu będzie pomoc");
-        LinearLayout ll = findViewById(R.id.helpLayout);
-        ll.addView(tv);
+        TextView helpTextView = findViewById(R.id.helpTextView);
+        String text = getString(R.string.helpText);
+        CharSequence styledText = Html.fromHtml(text);
+        //Sets textView with html markups
+        helpTextView.setText(styledText);
     }
 }
